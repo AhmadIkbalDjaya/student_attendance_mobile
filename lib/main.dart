2a2c361ac12a6d653e3a8adfa1621237c6a25db7
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-10-12 01:56:28 +0800

    feat: detail, edit teacher ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:student_attendance/pages/admin/teacher/CreateTeacher.dart';
+import 'package:student_attendance/pages/admin/teacher/EditTeacher.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CreateTeacherPage(),
+      home: EditTeacherPage(),
     );
   }
 }
---
+++ lib/pages/admin/teacher/DetailTeacher.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:student_attendance/components/MyAppBar.dart';
+import 'package:student_attendance/components/MyDrawer.dart';
+
+class DetailTeacherPage extends StatelessWidget {
+  const DetailTeacherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const MyAppBar(),
+      drawer: const MyDrawer(),
+      body: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Color(0xFFD9D9D9),
+            ),
+            padding:
+                const EdgeInsets.only(top: 0, bottom: 10, right: 10, left: 10),
+            width: double.infinity,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Informasi Guru",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Datail Informasi Guru",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 15,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 25,
+                    horizontal: 30,
+                  ),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    color: Color(0xFFD9D9D9),
+                  ),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Ahmad Ikbal Djaya",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const Text(
+                        "60200120073",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Row(
+                        children: [
+                          Icon(
+                            Icons.email_outlined,
+                            size: 25,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "ikbaldjaya@gmail.com",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      const Row(
+                        children: [
+                          Icon(
+                            Icons.phone,
+                            size: 25,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "081241250245",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      const Row(
+                        children: [
+                          Icon(
+                            Icons.male,
+                            size: 25,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "Laki-Laki",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.amber[300],
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 0,
+                              ),
+                            ),
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.edit_square,
+                                  color: Colors.black,
+                                  size: 20,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  "Edit",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 15),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 0,
+                              ),
+                            ),
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.delete,
+                                  color: Colors.black,
+                                  size: 20,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  "Hapus",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
